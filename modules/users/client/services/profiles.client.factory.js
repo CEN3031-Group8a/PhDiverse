847ae2a05ae6e953ac5f7d018e083642b670ea7a
Richard Li <li.richard.2013@gmail.com>
2016-02-20 17:14:46 -0500

    app has found users database but is forbidden from accessing

--- modules/users/client/services/profiles.client.factory.js
+++ modules/users/client/services/profiles.client.factory.js
@@ -2,23 +2,23 @@ angular.module('users').factory('Profiles', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:8080/api/profiles');
+        return $http.get('http://localhost:8080/api/users');
       },
 
       create: function(profile) {
-        return $http.post('http://localhost:8080/api/profiles', profile);
+        return $http.post('http://localhost:8080/api/users', profile);
       }, 
 
       read: function(id) {
-        return $http.get('http://localhost:8080/api/profiles/' + id);
+        return $http.get('http://localhost:8080/api/users/' + id);
       }, 
 
       update: function(id, profile) {
-        return $http.put('http://localhost:8080/api/profiles/' + id, profile);
+        return $http.put('http://localhost:8080/api/users/' + id, profile);
       }, 
 
       delete: function(id) {
-        return $http.delete('http://localhost:8080/api/profiles/' + id);
+        return $http.delete('http://localhost:8080/api/users/' + id);
       }
     };
 
