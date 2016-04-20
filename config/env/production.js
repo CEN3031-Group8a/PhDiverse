b991fc8ef6db4ad6fe64cc02b1977157de290dc0
“Justin <JDJacinto29@ufl.edu>
2016-04-19 20:59:51 -0400

    linkedin test 13

--- config/env/production.js
+++ config/env/production.js
@@ -56,7 +56,7 @@ module.exports = {
   linkedin: {
     clientID: process.env.LINKEDIN_ID || 'APP_ID',
     clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
-    callbackURL: 'http://phdiverse.herokuapp.com/api/auth/linkedin/callback'
+    callbackURL: 'https://oauth.io/auth'
   },
   github: {
     clientID: process.env.GITHUB_ID || 'APP_ID',
